cff6ef6a44a9393a7ee352c2d1ecc452912b3aac
Sumankumar <sandirisuman3@gmail.com>
2023-11-01 12:28:27 +0530

    reactone

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div>
+          <h1>TechyStar</h1>
+          <p>@all right reserved</p>
+        </div>
+
+        <div>
+          <div>
+            <h5>Follow Us</h5>
+            <a href="http:youtube.com" target="{blank}">
+              youtube
+            </a>
+            <a href="http:youtube.com" target="{blank}">
+              youtube
+            </a>
+            <a href="http:youtube.com" target="{blank}">
+              youtube
+            </a>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
